928bd469c7faddd0b8061bc2f63cdbf61f7722d2
AubreyRhea <aubrhea@gmail.com>
2017-11-25 17:57:43 -0600

    Add files via upload

--- js/contact_me.js
+++ js/contact_me.js
@@ -6,52 +6,54 @@ $(function() {
             // additional error messages or events
         },
         submitSuccess: function($form, event) {
-            event.preventDefault(); // prevent default submit behaviour
-            // get values from FORM
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var phone = $("input#phone").val();
-            var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
-            // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(' ') >= 0) {
-                firstName = name.split(' ').slice(0, -1).join(' ');
-            }
-            $.ajax({
-                url: "//formspree.io/atxgisday@gmail.com",
-                method: "POST",
-                data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message,
-		    dataType: "json"
-                },
-                cache: false,
-                success: function(data) {
-                    // Success message
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
+			if (event.target.id == "contactForm") {
+				event.preventDefault(); // prevent default submit behaviour
+				// get values from FORM
+				var name = $("input#name").val();
+				var email = $("input#email").val();
+				var phone = $("input#phone").val();
+				var message = $("textarea#message").val();
+				var firstName = name; // For Success/Failure Message
+				// Check for white space in name for Success/Fail message
+				if (firstName.indexOf(' ') >= 0) {
+					firstName = name.split(' ').slice(0, -1).join(' ');
+				}
+				$.ajax({
+					url: "//formspree.io/atxgisday@gmail.com",
+					method: "POST",
+					data: {
+						name: name,
+						phone: phone,
+						email: email,
+						message: message,
+				dataType: "json"
+					},
+					cache: false,
+					success: function(data) {
+						// Success message
+						$('#success').html("<div class='alert alert-success'>");
+						$('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+							.append("</button>");
+						$('#success > .alert-success')
+							.append("<strong>Your message has been sent. </strong>");
+						$('#success > .alert-success')
+							.append('</div>');
 
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-                error: function(err) {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-            })
+						//clear all fields
+						$('#contactForm').trigger("reset");
+					},
+					error: function(err) {
+						// Fail message
+						$('#success').html("<div class='alert alert-danger'>");
+						$('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+							.append("</button>");
+						$('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+						$('#success > .alert-danger').append('</div>');
+						//clear all fields
+						$('#contactForm').trigger("reset");
+					},
+				})
+			}
         },
         filter: function() {
             return $(this).is(":visible");
